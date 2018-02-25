8b7964d12a8f63548a569e1562e7985611ffee1f
Steph <sblencke@hotmail.com>
2018-02-25 14:52:33 +0000

    Added in ng project

--- Recipes/Recipes/Startup.cs
+++ Recipes/Recipes/Startup.cs
@@ -25,10 +25,7 @@ namespace Recipes
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseStaticFiles();
         }
     }
 }
