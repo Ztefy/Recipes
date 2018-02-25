11492bf0092e5cef83f9aa9fe15b3ace131d23e1
Steph <sblencke@hotmail.com>
2018-02-25 16:55:37 +0000

    Setup index page with menu & basic styling

---
+++ Recipes/Recipes/Controllers/AppController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recipes.Controllers
+{
+    public class AppController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Recipe()
+        {
+            ViewBag.Title = "Recipe";
+
+            return View();
+        }
+    }
+}
--- Recipes/Recipes/Startup.cs
+++ Recipes/Recipes/Startup.cs
@@ -15,6 +15,7 @@ namespace Recipes
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,8 +25,19 @@ namespace Recipes
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseExceptionHandler("/error");
+            }
 
             app.UseStaticFiles();
+
+            app.UseMvc(cfg =>
+            {
+            cfg.MapRoute("Default",
+                "{controller}/{action}/{id?}",
+                new { controller = "App", Action = "Index" });
+            });
         }
     }
 }
